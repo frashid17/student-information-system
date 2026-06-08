e0a05d73c72e37f5dff86ff743d008349c31ac09
Patrick Mwangi <patrick.mwangi@kemu.ac.ke>
2026-06-09 00:54:11 +0300

    Add application bootstrap and helper includes

---
+++ includes/init.php
@@ -0,0 +1,7 @@
+<?php
+
+require_once __DIR__ . '/../config/app.php';
+require_once __DIR__ . '/auth.php';
+require_once __DIR__ . '/units_helpers.php';
+require_once __DIR__ . '/password_helpers.php';
+require_once __DIR__ . '/gpa_helpers.php';
