14fd454bf41f56a7532652ec1296cdb7062eb093
frashid17 <frashid274@gmail.com>
2026-06-09 01:35:35 +0300

    Load staff teaching helpers during application bootstrap.

--- includes/init.php
+++ includes/init.php
@@ -5,3 +5,4 @@ require_once __DIR__ . '/auth.php';
 require_once __DIR__ . '/units_helpers.php';
 require_once __DIR__ . '/password_helpers.php';
 require_once __DIR__ . '/gpa_helpers.php';
+require_once __DIR__ . '/staff_helpers.php';
